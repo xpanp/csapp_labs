4d05ec60b6ca6729f113b2dd63129224a456c80f
xpanp <852290039@qq.com>
2022-11-14 10:29:41 +0800

    add transpose answer

--- labs/cachelab-handout/ans.md
+++ labs/cachelab-handout/ans.md
@@ -1,11 +1,17 @@
 # cache lab ans
 
-这边实现较为简单，不做详细说明。
-
 ## Part A: Cache Simulator
+
+这部分实现较为简单，不做详细说明。
 
 不使用全局变量，而是用一个结构体`cache_ctl`来包含所有需要的数据。
 
 `lru`使用计数来实现，每读取一次操作时间戳加一，因此时间戳最小的就是最近最久未使用的。
 
 具体实现见代码。
+
+## Part B: Optimizing Matrix Transpose
+
+这部分较难，参考[CSAPP实验之cache lab](https://zhuanlan.zhihu.com/p/79058089), [CSAPP - Cache Lab的更(最)优秀的解法](https://zhuanlan.zhihu.com/p/387662272)。
+
+主要需要想清楚一个blocking在cache中占用的set是间隔的。比如`32*32`的数组，此时以`8*8`分块，则该块在cache中是每4个set占用一个，一共占用8个。
--- labs/cachelab-handout/trans.c
+++ labs/cachelab-handout/trans.c
@@ -22,6 +22,140 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == 32) {
+        int i, j, m;
+        int t1, t2, t3, t4, t5, t6, t7, t8;
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                for (m = i; m < i + 8; ++m) {
+                    t1 = A[m][j];
+                    t2 = A[m][j+1];
+                    t3 = A[m][j+2];
+                    t4 = A[m][j+3];
+                    t5 = A[m][j+4];
+                    t6 = A[m][j+5];
+                    t7 = A[m][j+6];			
+                    t8 = A[m][j+7];
+                    B[j][m] = t1;
+                    B[j+1][m] = t2;
+                    B[j+2][m] = t3;
+                    B[j+3][m] = t4;
+                    B[j+4][m] = t5;
+                    B[j+5][m] = t6;
+                    B[j+6][m] = t7;
+                    B[j+7][m] = t8;
+                }
+            }
+        }
+    } else if (M == 64) {
+        int i, j, x, y;
+        int t1, t2, t3, t4, t5, t6, t7, t8;
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                for (x = i; x < i + 4; ++x)
+                {
+                    t1 = A[x][j]; 
+                    t2 = A[x][j+1]; 
+                    t3 = A[x][j+2]; 
+                    t4 = A[x][j+3];
+                    t5 = A[x][j+4]; 
+                    t6 = A[x][j+5]; 
+                    t7 = A[x][j+6]; 
+                    t8 = A[x][j+7];
+                    
+                    B[j][x] = t1; 
+                    B[j+1][x] = t2; 
+                    B[j+2][x] = t3; 
+                    B[j+3][x] = t4;
+                    B[j][x+4] = t5; 
+                    B[j+1][x+4] = t6; 
+                    B[j+2][x+4] = t7; 
+                    B[j+3][x+4] = t8;
+                }
+
+                for (y = j; y < j + 4; ++y)
+                {
+                    t1 = A[i+4][y]; 
+                    t2 = A[i+5][y]; 
+                    t3 = A[i+6][y]; 
+                    t4 = A[i+7][y];
+                    t5 = B[y][i+4]; 
+                    t6 = B[y][i+5]; 
+                    t7 = B[y][i+6]; 
+                    t8 = B[y][i+7];
+                    
+                    B[y][i+4] = t1; 
+                    B[y][i+5] = t2; 
+                    B[y][i+6] = t3; 
+                    B[y][i+7] = t4;
+                    B[y+4][i] = t5; 
+                    B[y+4][i+1] = t6; 
+                    B[y+4][i+2] = t7; 
+                    B[y+4][i+3] = t8;
+                }
+
+                for (x = i + 4; x < i + 8; ++x)
+                {
+                    t1 = A[x][j+4]; 
+                    t2 = A[x][j+5]; 
+                    t3 = A[x][j+6]; 
+                    t4 = A[x][j+7];
+                    B[j+4][x] = t1; 
+                    B[j+5][x] = t2; 
+                    B[j+6][x] = t3; 
+                    B[j+7][x] = t4;
+                }
+            }
+        } 
+    } else if (M == 61) {
+        int i, j;
+        int n = N / 8 * 8;
+		int m = M / 8 * 8;
+        int t1, t2, t3, t4, t5, t6, t7, t8;
+        for (j = 0; j < m; j += 8) {
+            for (i = 0; i < n; ++i) {
+				t1 = A[i][j];
+				t2 = A[i][j+1];
+				t3 = A[i][j+2];
+				t4 = A[i][j+3];
+				t5 = A[i][j+4];
+				t6 = A[i][j+5];
+				t7 = A[i][j+6];
+				t8 = A[i][j+7];
+				
+				B[j][i] = t1;
+				B[j+1][i] = t2;
+				B[j+2][i] = t3;
+				B[j+3][i] = t4;
+				B[j+4][i] = t5;
+				B[j+5][i] = t6;
+				B[j+6][i] = t7;
+				B[j+7][i] = t8;
+			}
+        }
+			
+		for (i = n; i < N; ++i) {
+            for (j = m; j < M; ++j) {
+				t1 = A[i][j];
+				B[j][i] = t1;
+			}
+        }
+
+		for (i = 0; i < N; ++i) {
+			for (j = m; j < M; ++j) {
+				t1 = A[i][j];
+				B[j][i] = t1;
+			}
+        }
+
+
+		for (i = n; i < N; ++i) {
+			for (j = 0; j < M; ++j) {
+				t1 = A[i][j];
+				B[j][i] = t1;
+			}
+        }
+    }
 }
 
 /* 
